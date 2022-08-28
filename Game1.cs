2a090fa135d1c0da0e5a2d8698e594af6c7dfdf1
GeorgePopaA <poplike14@yahoo.com>
2022-08-28 16:47:38 +0300

    I created and organised the resource for game, I setup the project and create necessary classes

---
+++ Game1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPG_Game
+{
+    enum Dir {
+        Down,
+        Up,
+        Left,
+        Right
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D player_Sprite;
+        Texture2D playerDown;
+        Texture2D playerUp;
+        Texture2D playerLeft;
+        Texture2D playerRight;
+
+        Texture2D eyeEnemy_Sprite;
+        Texture2D snakeEnemy_Sprite;
+        Texture2D brush_Sprite;
+        Texture2D tree_Sprite;
+
+        Texture2D heart_Sprite;
+        Texture2D bullet_Sprite;
+
+        Player player = new Player();
+        Vector2 position_test = new Vector2(50, 50);
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 700;
+            _graphics.ApplyChanges();
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            playerDown = Content.Load<Texture2D>("Player/playerDown");
+            playerUp = Content.Load<Texture2D>("Player/playerUp");
+            playerLeft = Content.Load<Texture2D>("Player/playerLeft");
+            playerRight = Content.Load<Texture2D>("Player/playerRight");
+            player_Sprite = Content.Load<Texture2D>("Player/player");
+
+            brush_Sprite = Content.Load<Texture2D>("Obstacle/bush");
+            tree_Sprite = Content.Load<Texture2D>("Obstacle/tree");
+
+            bullet_Sprite = Content.Load<Texture2D>("Misc/bullet");
+            heart_Sprite = Content.Load<Texture2D>("Misc/heart");
+
+            eyeEnemy_Sprite = Content.Load<Texture2D>("Enemies/eyeEnemy");
+            snakeEnemy_Sprite = Content.Load<Texture2D>("Enemies/snakeEnemy");
+
+            player.animations[0] = new AnimatedSprite(playerDown, 1, 4);
+            player.animations[1] = new AnimatedSprite(playerUp, 1, 4);
+            player.animations[2] = new AnimatedSprite(playerLeft, 1, 4);
+            player.animations[3] = new AnimatedSprite(playerRight, 1, 4);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            player.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.ForestGreen);
+
+            player.anim.Draw(_spriteBatch, player.Position);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPG_Game
+{
+    class Player
+    {
+        private Vector2 position = new Vector2(100, 100);
+        private int health = 3;
+        private int speed = 200;
+        private Dir direction = Dir.Down;
+        private bool isMooving = false;
+        
+        public AnimatedSprite anim;
+        public AnimatedSprite[] animations = new AnimatedSprite[4];
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+        }
+
+        public void setX (float newX)
+        {
+            position.X = newX;
+        }
+        public void setY(float newY)
+        {
+            position.Y = newY;
+        }
+
+        public void Update(GameTime gt)
+        {
+            KeyboardState ks = Keyboard.GetState();
+            float dt = (float)gt.ElapsedGameTime.TotalSeconds;
+            isMooving = false;
+            
+            anim = animations[(int)direction];
+            
+            if (isMooving)
+            {
+                anim.Update(gt);
+            }
+            
+
+            if (ks.IsKeyDown(Keys.Right))
+            {
+                direction = Dir.Right;
+                isMooving = true;
+            }
+            if (ks.IsKeyDown(Keys.Left))
+            {
+                direction = Dir.Left;
+                isMooving = true;
+            }
+            if (ks.IsKeyDown(Keys.Up))
+            {
+                direction = Dir.Up;
+                isMooving = true;
+            }
+            if (ks.IsKeyDown(Keys.Down))
+            {
+                direction = Dir.Down;
+                isMooving = true;
+            }
+
+            if (isMooving)
+            {
+                switch (direction)
+                {
+                    case Dir.Right:
+                        position.X += speed * dt;
+                        break;
+                    case Dir.Left:
+                        position.X -= speed * dt;
+                        break;
+                    case Dir.Up:
+                        position.Y -= speed * dt;
+                        break;
+                    case Dir.Down:
+                        position.Y += speed * dt;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
